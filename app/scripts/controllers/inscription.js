1343cc59ef365fbba4800998578dc4b238d52cca
N'zi Etranse Landry-Desire <landry.desire.nzi@live.fr>
2015-12-22 15:49:08 +0100

    bug inscription

--- app/scripts/controllers/inscription.js
+++ app/scripts/controllers/inscription.js
@@ -2,26 +2,22 @@
 
 angular.module('Inscription')
   .controller('InscriptionCtrl', ['$scope','$http',function ($scope,$http) {
-    $scope.user = "";
     $scope.email = "email";
     $scope.pwd = "pwd";
     $scope.confpwd= "confpwd"
-     $http({
-    	method:"post",
-    	url:"http://localhost:8080/ecommerce_api/users/addUser",
-    	data:JSON.stringify({
-    		email: $scope.email,
-	        mdp: $scope.pwd	
-    		})
-    	})
-     .success(
-    	function(result){
-    		console.log(result);
-    	}
-    	);
     $scope.inscription = function(){
-    	console.log('>> ',$scope.email,$scope.pwd,$scope.confpwd);
-    	//user = $scope.form;
-    	console.log("user"+user);
+    	$http({
+        method:"post",
+        url:"/ws/ecommerce_api/users/addUser",
+        data:JSON.stringify({
+            email: $scope.email,
+            mdp: $scope.pwd 
+            })
+        })
+     .success(
+        function(result){
+            console.log(result);
+        }
+        );
     }
   }]);
